a7fde84fd0ab1c794fff96f19e1e1d148a1f8b2f
Nathan <nunya@example.com>
2025-09-18 16:14:59 -0400

    start implementing drivetrain class :fire:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Drivetrain.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.dhs.components;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
 public class Drivetrain {
+    public DcMotor FRMotor;
+    public DcMotor FLMotor;
+    public DcMotor BRMotor;
+    public DcMotor BLMotor;
+
+    public IMU imu;
+
+    // Field-Oriented Drive
+    public void fodDrive(double turnVal, double driveX, double driveY) {
+
+    }
+
+    // Robot-Oriented Drive
+    public void rodDrive(double turnVal, double driveX, double driveY) {
+
+    }
 }
