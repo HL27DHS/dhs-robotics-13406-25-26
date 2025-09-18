da2128037fdb0c1c68dd628b1c54f47323294c9e
Nathan <nunya@example.com>
2025-09-18 15:58:53 -0400

    Create base (unimplemented) classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/Bot.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.dhs;
+
+import org.firstinspires.ftc.teamcode.dhs.components.Drivetrain;
+import org.firstinspires.ftc.teamcode.dhs.components.Launcher;
+import org.firstinspires.ftc.teamcode.dhs.components.Peripherals;
+import org.firstinspires.ftc.teamcode.dhs.components.Spintake;
+
+public class Bot {
+    Drivetrain drivetrain;
+    Peripherals peripherals;
+    Launcher launcher;
+    Spintake spintake;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Launcher.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.dhs.components;
+
+public class Launcher {
+}
