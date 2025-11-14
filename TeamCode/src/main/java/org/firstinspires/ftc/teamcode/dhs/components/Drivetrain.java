d58040a856c07646c50a7525247b1393105c50d4
Nathan G. <rznake80@gmail.com>
2025-11-13 20:44:25 -0500

    Added functions to get each motor, renamed parameters for better readability and added Javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Drivetrain.java
@@ -28,8 +28,8 @@ public class Drivetrain {
     public void fodDrive(double turnVal, double driveX, double driveY) {
         double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-        double rotX = driveX * Math.cos(-botHeading) - driveY * Math.sin(-botHeading);
-        double rotY = driveX * Math.sin(-botHeading) + driveY * Math.cos(-botHeading);
+        double rotX = strafe * Math.cos(-botHeading) - forward * Math.sin(-botHeading);
+        double rotY = strafe * Math.sin(-botHeading) + forward * Math.cos(-botHeading);
 
         double modifiedX = rotX * 1.1; // set modifier as needed
 
@@ -47,15 +47,25 @@ public class Drivetrain {
         drive.rightBack.setPower(br);
     }
 
-    public void rodDrive(double turnVal, double driveX, double driveY) {
-        double modifiedX = driveX * 1.1; // set modifier as needed
-        double denominator = Math.max(Math.abs(driveY) + Math.abs(modifiedX) + Math.abs(turnVal), 1);
+    /**
+     * Calculates and executes Mecanum Robot-Oriented Drive values given the below parameters
+     *
+     * @param turnVal The rate at which the bot should turn between -1 (cw) and 1 (ccw),
+     *                for example, a right stick x value.
+     * @param strafe The bot's strafe or side-to-side drive power between -1 (left) and 1 (right),
+     *               for example, a left stick x value.
+     * @param forward The bot's forward and back drive power between -1 (back) and 1 (forward),
+     *               for example, a left stick y value.
+     */
+    public void rodDrive(double turnVal, double strafe, double forward) {
+        double modifiedX = strafe * 1.1; // set modifier as needed
+        double denominator = Math.max(Math.abs(forward) + Math.abs(modifiedX) + Math.abs(turnVal), 1);
 
         //          Forward     Strafe     Turn
-        double fl = (driveY + modifiedX + turnVal) / denominator;
-        double bl = (driveY - modifiedX + turnVal) / denominator;
-        double fr = (driveY - modifiedX - turnVal) / denominator;
-        double br = (driveY + modifiedX - turnVal) / denominator;
+        double fl = (forward + modifiedX + turnVal) / denominator;
+        double bl = (forward - modifiedX + turnVal) / denominator;
+        double fr = (forward - modifiedX - turnVal) / denominator;
+        double br = (forward + modifiedX - turnVal) / denominator;
 
         drive.leftFront.setPower(fl);
         drive.leftBack.setPower(bl);
