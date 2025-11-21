3505ca43c9fbd4a780d4a854d21d17c90ceb0a3b
Nathan <nunya@example.com>
2025-11-21 09:49:02 -0500

    Add cycle motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Spintake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Spintake.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Spintake {
     public DcMotor spintakeMotor;
+    public DcMotor cycleMotor;
     public Servo sortServo;
 
     // TODO: Figure out servo sorter positions
@@ -17,9 +18,13 @@ public class Spintake {
 
     public Spintake(HardwareMap hardwareMap) {
         spintakeMotor = hardwareMap.get(DcMotor.class, "spintake");
+        cycleMotor = hardwareMap.get(DcMotor.class, "cycle");
         sortServo = hardwareMap.get(Servo.class, "sorter");
 
         spintakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        spintakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        cycleMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         closeSort();
     }
@@ -27,6 +32,7 @@ public class Spintake {
     public void setSpintakePower(double power) {
         spintakeMotor.setPower(power);
     }
+    public void setCyclePower(double power) { cycleMotor.setPower(power); }
 
     public void closeSort() {
         sortServo.setPosition(sortClosePos);
