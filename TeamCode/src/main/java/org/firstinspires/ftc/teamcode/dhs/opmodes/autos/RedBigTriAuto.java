b17ed465842df293ed2270a5ae22aac0e0000642
nate <147215321+Rattlznake@users.noreply.github.com>
2025-11-23 13:05:53 -0500

    Create red & blue side autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/autos/RedBigTriAuto.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.dhs.opmodes.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.dhs.components.PrimitiveDrive;
+
+@Autonomous(name="Red Big Triangle Auto",group="Terrible Terrible Awful")
+public class RedBigTriAuto extends LinearOpMode {
+    PrimitiveDrive drivetrain;
+
+    public void runOpMode() {
+        drivetrain = new PrimitiveDrive(hardwareMap);
+
+        waitForStart();
+
+        // Go forward for 1 second
+        drivetrain.rodDrive(0,0,1);
+        sleep(1000);
+
+        // Go left for 0.5 seconds
+        drivetrain.rodDrive(0,-1,0);
+        sleep(500);
+
+        // Stop the bot
+        drivetrain.getFlMotor().setPower(0);
+        drivetrain.getFrMotor().setPower(0);
+        drivetrain.getBlMotor().setPower(0);
+        drivetrain.getBrMotor().setPower(0);
+    }
+}
