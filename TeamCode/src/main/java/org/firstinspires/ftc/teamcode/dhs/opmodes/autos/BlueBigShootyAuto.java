769b325f9b0fb1eace271bf0dfeef0526d6d6646
nate <147215321+Rattlznake@users.noreply.github.com>
2025-11-23 13:05:33 -0500

    Created non-functional auto that is supposed to shoot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/autos/BlueBigShootyAuto.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.dhs.opmodes.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.dhs.components.Launcher;
+import org.firstinspires.ftc.teamcode.dhs.components.PrimitiveDrive;
+import org.firstinspires.ftc.teamcode.dhs.components.Spintake;
+
+@Autonomous(name="Blue Big Triangle Shooty Auto",group="Terrible Terrible Awful")
+public class BlueBigShootyAuto extends LinearOpMode {
+    PrimitiveDrive drivetrain;
+    Launcher launcher;
+    Spintake spintake;
+
+    public void runOpMode() {
+        // initialize the stuff
+        drivetrain = new PrimitiveDrive(hardwareMap);
+        launcher = new Launcher(hardwareMap);
+        spintake = new Spintake(hardwareMap);
+
+        // Wait for start
+        waitForStart();
+
+        // Go back for 1 seconds
+        drivetrain.rodDrive(0, 0, -0.8);
+        sleep(1000);
+        drivetrain.rodDrive(0,0,0);
+
+        // Prepare to shoot first ball
+        launcher.setFlywheelPower(1);
+        sleep(2000);
+
+        // Spin the cycler for 1/4th of a second to fire the first ball
+        spintake.setCyclePower(1);
+        sleep(250);
+        spintake.setCyclePower(0);
+
+        // Wait two seconds for the flywheel to reach speed
+        sleep(2000);
+
+        // Spin the cycler for 1/4th of a second to fire the second ball
+        spintake.setCyclePower(1);
+        sleep(250);
+        spintake.setCyclePower(0);
+
+        // Wait two seconds for the flywheel to reach speed
+        sleep(2000);
+
+        // Spin the cycler for 1/4th of a second to fire the third ball
+        spintake.setCyclePower(1);
+        sleep(250);
+        spintake.setCyclePower(0);
+
+        // Stop flywheel, it doesn't need to spin any more.
+        launcher.setFlywheelPower(0);
+
+        // Move left for 0.5 seconds to get out of the way
+        drivetrain.rodDrive(0,-1,0);
+        sleep(500);
+
+        // Stop all motors
+        spintake.setSpintakePower(0);
+        spintake.setCyclePower(0);
+        launcher.setFlywheelPower(0);
+
+        drivetrain.getFlMotor().setPower(0);
+        drivetrain.getFrMotor().setPower(0);
+        drivetrain.getBlMotor().setPower(0);
+        drivetrain.getBrMotor().setPower(0);
+    }
+}
