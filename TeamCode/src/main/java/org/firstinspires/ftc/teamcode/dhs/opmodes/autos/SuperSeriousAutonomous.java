2aa15dfadb870bfebdd9a6300d196090f8947d34
nate <147215321+Rattlznake@users.noreply.github.com>
2025-12-20 14:00:48 -0500

    make some changes to SuperSeriousAutonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/autos/SuperSeriousAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/autos/SuperSeriousAutonomous.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.dhs.opmodes.autos;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -13,19 +15,23 @@ import org.firstinspires.ftc.teamcode.dhs.components.*;
 @Autonomous(name="Super Serious Autonomous")
 public class SuperSeriousAutonomous extends LinearOpMode {
 
-
     public void runOpMode() {
         // Init code here
         Drivetrain drivetrain = new Drivetrain(hardwareMap);
         Launcher launcher = new Launcher(hardwareMap);
         Spintake spintake = new Spintake(hardwareMap);
 
-        Pose2d initialPose = new Pose2d(0,0, 0);
+        // "somethin' eleven" - James Fonseca 2025
+        Pose2d initialPose = new Pose2d(-40,54, 0);
         MecanumDrive rrDrive = new MecanumDrive(hardwareMap, initialPose);
 
         // very simple spline
         TrajectoryActionBuilder start = rrDrive.actionBuilder(initialPose)
-                        .splineTo(new Vector2d(48, 48), Math.PI / 2);
+                        .splineTo(new Vector2d(0, 0), 3 * Math.PI / 2,
+                                new TranslationalVelConstraint(30),
+                                new ProfileAccelConstraint(-40, 50))
+                        .splineTo(new Vector2d(60, 0), 0);
+
 
         waitForStart();
         // Do Stuff code here
