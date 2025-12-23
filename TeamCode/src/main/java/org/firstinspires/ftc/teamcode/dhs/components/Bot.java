aa014e8cb7fee2779060eadd8b81cfa7db11e430
Nathan <nunya@example.com>
2025-12-23 10:06:53 -0500

    fix poses & add setAllMotorsZeroPowerBehavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/components/Bot.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.dhs.components;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -20,24 +21,44 @@ public class Bot {
 
     public Team team;
 
-    public Bot(HardwareMap hardwareMap) {
+    public Bot(HardwareMap hardwareMap, Team team) {
         drivetrain = new Drivetrain(hardwareMap);
         spintake = new Spintake(hardwareMap);
         launcher = new Launcher(hardwareMap);
+
+        this.team = team;
     }
 
     public Pose2d getDepotPosition() {
         // TODO: Implement function that gets the Pose2d of the Depot
         if (team == Team.BLUE)
-            return new Pose2d(0,0,0);
+            return new Pose2d(-60,-60,0);
         if (team == Team.RED)
-            return new Pose2d(0,0,0);
+            return new Pose2d(-60,60,0);
 
         return new Pose2d(0,0,0);
     }
 
     public double getAngleToFaceDepot(AngleUnit unit) {
         // TODO: Implement function that gets the bot's needed heading to face the depot
-        return 0;
+        Pose2d currentPosition = drivetrain.getDrive().localizer.getPose();
+        Pose2d depotPosition = getDepotPosition();
+
+        double deltaX = depotPosition.position.x - currentPosition.position.x;
+        double deltaY = depotPosition.position.y - currentPosition.position.y;
+
+        double angle = Math.atan2(deltaY, deltaX);
+
+        if (unit == AngleUnit.DEGREES)
+            return Math.toDegrees(angle);
+
+        return angle;
+    }
+
+    public void setAllMotorsZeroPowerBehavior(DcMotor.ZeroPowerBehavior behavior) {
+        drivetrain.getFrMotor().setZeroPowerBehavior(behavior);
+        drivetrain.getFlMotor().setZeroPowerBehavior(behavior);
+        drivetrain.getBrMotor().setZeroPowerBehavior(behavior);
+        drivetrain.getBlMotor().setZeroPowerBehavior(behavior);
     }
 }
