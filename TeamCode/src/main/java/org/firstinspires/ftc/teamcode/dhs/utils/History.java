8d83ddff1ab990985b85ef94db5b5d7296e980f9
nate <147215321+Rattlznake@users.noreply.github.com>
2026-01-02 11:21:27 -0500

    Finish implementing toList() and toArray() in the History class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/History.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/History.java
@@ -30,7 +30,17 @@ public class History<T> {
         timeline.clear();
     }
 
-    public T[] toArray() {
-        // TODO: Implement
-    }
+    /**
+     * Get the history list, stored from newest to oldest
+     * @return the list
+     */
+    public List<T> toList() { return timeline; }
+
+    /**
+     * Get the history list as an array, sorted from newest to oldest
+     * @return the array
+     */
+    @SuppressWarnings("unchecked") // In this case, the unchecked cast is fine because we don't
+                                   // actually do anything with the array we created.
+    public T[] toArray() { return timeline.toArray( (T[]) new Object[0] ); }
 }
