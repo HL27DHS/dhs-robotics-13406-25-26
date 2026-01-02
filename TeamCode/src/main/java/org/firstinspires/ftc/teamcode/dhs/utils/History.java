7b117995f2a1121b2793a42e65cefb2bdc0a6eda
nate <147215321+Rattlznake@users.noreply.github.com>
2026-01-02 11:25:50 -0500

    rename "timeline" to more applicable name "points", representing points in history

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/History.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/History.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class History<T> {
-    /** The timeline containing each point kept in history */
-    private final List<T> timeline;
+    /** The ArrayList containing each point in history */
+    private final List<T> points;
 
     /** How many points in history the timeline keeps */
     private final int depth;
@@ -16,7 +16,7 @@ public class History<T> {
      */
     public History(int depth) {
         this.depth = depth;
-        this.timeline = new ArrayList<T>();
+        this.points = new ArrayList<T>();
     }
 
     /**
@@ -30,31 +30,31 @@ public class History<T> {
      * @param item the item to add
      */
     public void add(T item) {
-        timeline.add(item);
+        points.add(item);
 
         // Make sure list isn't too large
-        if (timeline.size() > depth)
-            timeline.subList(depth, timeline.size()).clear();
+        if (points.size() > depth)
+            points.subList(depth, points.size()).clear();
     }
 
     /**
      * Reset the kept history back to being empty
      */
     public void clear() {
-        timeline.clear();
+        points.clear();
     }
 
     /**
-     * Get the history list, stored from newest to oldest
+     * Get the points in history, stored from newest to oldest
      * @return the list
      */
-    public List<T> toList() { return timeline; }
+    public List<T> toList() { return points; }
 
     /**
-     * Get the history list as an array, sorted from newest to oldest
+     * Get the stored points in history as an array, sorted from newest to oldest
      * @return the array
      */
     @SuppressWarnings("unchecked") // In this case, the unchecked cast is fine because we don't
                                    // actually do anything with the array we created.
-    public T[] toArray() { return timeline.toArray( (T[]) new Object[0] ); }
+    public T[] toArray() { return points.toArray( (T[]) new Object[0] ); }
 }
