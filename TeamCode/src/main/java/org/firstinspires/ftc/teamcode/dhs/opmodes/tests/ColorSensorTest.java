0b95b9adf6d963b647d4c7081aacf89609896042
Nathan <nunya@example.com>
2026-01-05 16:25:16 -0500

    Create OpMode for testing color sensor values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/tests/ColorSensorTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.dhs.opmodes.tests;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.dhs.components.ColorSensor;
+import org.firstinspires.ftc.teamcode.dhs.utils.smartcontroller.SmartController;
+
+@TeleOp(name="Color Sensor Test",group="Testing Programs")
+public class ColorSensorTest extends OpMode {
+    ColorSensor sensor;
+    SmartController controller1;
+    double gainStep = 0.1;
+
+    public void init() {
+        sensor = new ColorSensor(hardwareMap);
+        controller1 = new SmartController();
+    }
+
+    public void loop() {
+        controller1.think(gamepad1);
+
+        if (controller1.dpadUp.justPressed())
+            sensor.sensor.setGain(sensor.sensor.getGain() + (float) gainStep);
+        if (controller1.dpadDown.justPressed())
+            sensor.sensor.setGain(sensor.sensor.getGain() - (float) gainStep);
+
+
+        telemetry.addData("color",sensor.rgbToString());
+        telemetry.addData("gain",sensor.sensor.getGain());
+        telemetry.update();
+    }
+}
