16ffd57ca2e91f9e6433c4642f3f6d36a9de83b2
Nathan G. <rznake80@gmail.com>
2026-01-05 19:08:37 -0500

    move SuperSeriousAutonomous into unstable package and disable it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/autos/unstable/SuperSeriousAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/autos/unstable/SuperSeriousAutonomous.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.dhs.opmodes.autos;
+package org.firstinspires.ftc.teamcode.dhs.opmodes.autos.unstable;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ProfileAccelConstraint;
@@ -7,11 +7,13 @@ import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.dhs.components.*;
 
+@Disabled
 @Autonomous(name="Super Serious Autonomous")
 public class SuperSeriousAutonomous extends LinearOpMode {
 
