c66f523b05f4809a158377e0c763ff3de50d51b6
Nathan G. <rznake80@gmail.com>
2026-01-26 15:56:45 -0500

    make OneDriverTeleOp use Bot class instead of separate classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/teleops/OneDriverTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/teleops/OneDriverTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.dhs.opmodes.teleops;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.dhs.components.Bot;
 import org.firstinspires.ftc.teamcode.dhs.components.Drivetrain;
 import org.firstinspires.ftc.teamcode.dhs.components.Launcher;
 import org.firstinspires.ftc.teamcode.dhs.components.PrimitiveDrive;
@@ -12,9 +13,7 @@ import org.firstinspires.ftc.teamcode.dhs.utils.smartcontroller.SmartUtils;
 
 @TeleOp(name="Ready Player One",group="A - Main Programs")
 public class  OneDriverTeleOp extends OpMode {
-    PrimitiveDrive drivetrain;
-    Spintake spintake;
-    Launcher launcher;
+    Bot bot;
 
     SmartController controller1 = new SmartController();
     SmartController controller2 = new SmartController();
@@ -35,9 +34,7 @@ public class  OneDriverTeleOp extends OpMode {
 
     @Override
     public void init() {
-        launcher = new Launcher(hardwareMap);
-        spintake = new Spintake(hardwareMap);
-        drivetrain = new PrimitiveDrive(hardwareMap);
+        bot = new Bot(hardwareMap);
     }
 
     @Override
@@ -48,7 +45,7 @@ public class  OneDriverTeleOp extends OpMode {
 
         // If Y pressed, reset FOD angle
         if (controller1.y.justPressed()) {
-            drivetrain.resetImuOffset();
+            bot.drivetrain.resetImuOffset();
         }
 
         // If X is pressed, reverse spintake & flywheel (this comes in handy more than you'd think)
@@ -67,16 +64,16 @@ public class  OneDriverTeleOp extends OpMode {
         double cyclePower = (controller1.rightTrigger.getValue() > 0.5) ? 1 : 0;
         double launchPower = (controller1.leftTrigger.getValue() > 0.5) ? (1 - launchModifierRange) + launchModifier : 0;
 
-        spintake.setSpintakePower(spintakePower * reverseModifier);
-        launcher.setCyclePower(cyclePower * reverseModifier);
-        launcher.setFlywheelPower(launchPower * reverseModifier);
+        bot.spintake.setSpintakePower(spintakePower * reverseModifier);
+        bot.launcher.setCyclePower(cyclePower * reverseModifier);
+        bot.launcher.setFlywheelPower(launchPower * reverseModifier);
 
         // If B is pressed, open the sort chute, if it's not, close it
         // Holding B will keep the sort chute open, letting go will close the sort chute
         if (controller1.b.isPressed())
-            spintake.openSort();
+            bot.spintake.openSort();
         else
-            spintake.closeSort();
+            bot.spintake.closeSort();
 
         // If both DPad Up and DPad Left are pressed, toggle FOD
         // Uses SmartUtils.combo to debounce buttons so that the combo is doable
@@ -86,13 +83,13 @@ public class  OneDriverTeleOp extends OpMode {
 
         // Do Robot-Oriented or Field-Oriented Drive
         if (useFod)
-            drivetrain.fodDrive(
+            bot.drivetrain.fodDrive(
                     controller1.rightStick.getX(),
                     controller1.leftStick.getX() * slowModeModifier,
                     -controller1.leftStick.getY() * slowModeModifier
             );
         else
-            drivetrain.rodDrive(
+            bot.drivetrain.rodDrive(
                     controller1.rightStick.getX(),
                     controller1.leftStick.getX() * slowModeModifier,
                     -controller1.leftStick.getY() * slowModeModifier
