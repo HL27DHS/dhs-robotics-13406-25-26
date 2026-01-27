c50dff54e1e2f8e5169dcbe8c2bcbe789d05e0c2
Nathan G. <rznake80@gmail.com>
2026-01-27 14:16:10 -0500

    add emaSmooth() for histories to HistoryUtils

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/HistoryUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/HistoryUtils.java
@@ -21,4 +21,33 @@ public class HistoryUtils {
 
         return slopes;
     }
+
+    /**
+     * Smooths the points of a (numerical) History
+     * @param history the history to smooth
+     * @param factor the EMA smoothing factor to use
+     * @param <T> the type of object that the history keeps track of
+     */
+    public static <T extends Number> void emaSmooth(History<T> history, double factor) {
+        // TODO: test this function, really sketchy casting
+        T[] originalTypeNums = history.getPointsArray();
+        double[] doubleNums = new double[originalTypeNums.length];
+
+        // cast original type numbers into doubles for smoothing
+        for (int i = 0; i < originalTypeNums.length; i++) {
+            doubleNums[i] = (double) originalTypeNums[i];
+        }
+
+        // smooth numbers and box them
+        Double[] doubleSmoothedNums = DataUtils.box(DataUtils.emaSmooth(doubleNums, factor));
+        T[] originalTypeSmoothedNums = (T[]) new Object[doubleSmoothedNums.length];
+
+        // cast doubles into original types for returning
+        for (int i = 0; i < doubleSmoothedNums.length; i++) {
+            originalTypeSmoothedNums[i] = (T) doubleSmoothedNums[i];
+        }
+
+        // reset history with new smoothed numbers
+        history.setPointsArray(originalTypeSmoothedNums);
+    }
 }
