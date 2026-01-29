dde100bf2851f4338c665662fc61ddfedf9c7973
Nathan G. <rznake80@gmail.com>
2026-01-28 20:14:29 -0500

    add distance() functions for calculating distance between Vector2ds and Pose2ds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/PoseUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/PoseUtils.java
@@ -4,5 +4,26 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 
 public class PoseUtils {
+    /**
+     * Calculate the distance between two {@code Vector2d}s.
+     * @param a The first vector
+     * @param b The second vector
+     * @return The distance between vectors {@code a} and {@code b}
+     */
+    public static double distance(Vector2d a, Vector2d b) {
+        double leg1 = Math.pow(a.x - b.x, 2);
+        double leg2 = Math.pow(a.y - b.y, 2);
 
+        return Math.sqrt(leg1 + leg2);
+    }
+
+    /**
+     * Calculate the distance between two {@code Pose2d}s
+     * @param a The first pose
+     * @param b The second pose
+     * @return The distance between poses {@code a} and {@code b}
+     */
+    public static double distance(Pose2d a, Pose2d b) {
+        return distance(a.position, b.position);
+    }
 }
