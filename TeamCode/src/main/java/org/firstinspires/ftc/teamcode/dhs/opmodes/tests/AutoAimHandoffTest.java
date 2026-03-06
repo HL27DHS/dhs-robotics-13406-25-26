aafa5f4f930d58c7fc79f722848ba082ab1d4edc
nate <147215321+Rattlznake@users.noreply.github.com>
2026-03-06 14:30:20 -0500

    Create AutoAimHandoffTest for testing the position passing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/opmodes/tests/AutoAimHandoffTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.dhs.opmodes.tests;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.dhs.components.Bot;
+import org.firstinspires.ftc.teamcode.dhs.game.Alliance;
+
+@Autonomous(name="Auto Aim Handoff Test", group="TestAuto", preselectTeleOp="AutoAimTest")
+public class AutoAimHandoffTest extends LinearOpMode {
+    Bot bot;
+    MecanumDrive rrDrive;
+
+    public void runOpMode() {
+        // Define poses used in autonomous
+        Pose2d initialPose = new Pose2d(63,-12, Math.PI);
+        Pose2d endPose = new Pose2d(30, -12, Math.PI);
+
+        bot = new Bot(hardwareMap, Alliance.BLUE, initialPose);
+        rrDrive = bot.drivetrain.getDrive();
+
+        // Define drive actions used in the autonomous
+        Action moveForward = rrDrive.actionBuilder(initialPose)
+                .strafeTo(endPose.position)
+                .build();
+
+        waitForStart();
+
+        // Actually run the actions yay
+        Actions.runBlocking(moveForward);
+    }
+}
