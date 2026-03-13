ae0e9aa243c9c85781d5fba6783f98329dfa92b5
Nathan G. <rznake80@gmail.com>
2026-03-13 18:17:48 -0400

    create unimplemented CanvasUtils class for drawing to canvases

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dhs/utils/CanvasUtils.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.dhs.utils;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.roadrunner.Vector2d;
+
+/**
+ * Includes various utility functions for drawing on {@link Canvas} instances
+ */
+public class CanvasUtils {
+    /**
+     * Draws a line on the canvas starting at start pos and continuing infinitely towards an angle
+     * @param canvas the canvas to draw on
+     * @param startPos the position to start the line at
+     * @param angle the angle the line go towards (radians)
+     */
+    public static void drawAimLine(Canvas canvas, Vector2d startPos, double angle) {
+        // TODO: Implement
+    }
+
+    /**
+     * Draws a point on the canvas
+     * @param canvas the canvas to draw on
+     * @param aimPos the position of the point on the canvas
+     */
+    public static void drawAimPoint(Canvas canvas, Vector2d aimPos) {
+        // TODO: Implement
+    }
+
+    /**
+     * Draws a representation of the bot on the canvas
+     * @param canvas the canvas to draw on
+     * @param botPos the position of the bot on the canvas
+     */
+    public static void drawBot(Canvas canvas, Vector2d botPos) {
+        // TODO: Implement
+    }
+
+    /**
+     * Draws an arrow on the canvas
+     * @param canvas the canvas to draw on
+     * @param startPos the starting point of the arrow
+     * @param endPos the end point of the arrow
+     * @param headSize the length of the head segments
+     */
+    public static void drawArrow(Canvas canvas, Vector2d startPos, Vector2d endPos, double headSize) {
+        // TODO: Implement
+    }
+}
